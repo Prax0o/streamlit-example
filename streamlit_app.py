e09d7cfa9f07bb9e01dbee10d8d65a819dc0e0a4
BENOIT Eliott <86134947+Prax0o@users.noreply.github.com>
2022-11-12 16:02:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,16 @@ st.set_page_config(page_title="ReviewAnalyzer Eliott BENOIT")
 ModelType = st.sidebar.radio("Choose your model",["Write by hand", "Choose from Dataset"])
 nb_topic = st.sidebar.number_input("Number of Topic",min_value=1,max_value=15)
 
+
 if(ModelType=="Write by hand"):
+  doc = st.text_area("write your comment below",height=510)
   st.write("Write by hand")
+  TEXT = doc
 else:
   st.write("Choose from Dataset")
 
 #main
 
-doc = st.text_area("write your comment below",height=510)
 
 if st.button(label="Search"):
   st.write('OK')
