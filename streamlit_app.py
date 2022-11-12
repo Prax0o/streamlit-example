bb77ae944223c9e89d290215586d0430f5c6472a
BENOIT Eliott <86134947+Prax0o@users.noreply.github.com>
2022-11-12 15:43:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,4 @@
 import streamlit as st
-import numpy as np
-from pandas import DataFrame
-from keybert import KeyBERT
-# For Flair (Keybert)
-from flair.embeddings import TransformerDocumentEmbeddings
-import seaborn as sns
-# For download buttons
-from functionforDownloadButtons import download_button
-import os
-import json
 
 st.set_page_config(page_title="ReviewAnalyzer Eliott BENOIT")
 
