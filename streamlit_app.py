997736798154d5efa10815c327c9e7f45a4bc375
BENOIT Eliott <86134947+Prax0o@users.noreply.github.com>
2022-11-12 15:46:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit as st
-from 
+from functions import topic_search
+
 st.set_page_config(page_title="ReviewAnalyzer Eliott BENOIT")
+
+#sidebar
 
 ModelType = st.sidebar.radio("Choose your model",["write", "choose"])
 nb_topic = st.sidebar.number_input("Number of Topic",min_value=1,max_value=15)
 
+#main
 
 doc = st.text_area("write your comment below",height=510)
 
 if st.button(label="Search"):
   st.write('OK')
 
- 
+
 
 st.write(topic_search(TEXT, 3, model=model, vect=vect))
